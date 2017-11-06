54aa329380f823702af23287c4c5da39e3ba28dc
Robert Enyiwork <enyiworkgonzalez100@gmail.com>
2017-11-06 15:26:11 -0500

    intro node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,19 @@
-var SortedList = function() {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+var newArr = ["single"];
+
+var SortedList = function() {
+
+};
+
+SortedList.prototype = function (){
+};
+SortedList.prototype.add     = function(item) {
+ var arr = item("news");
+};
+SortedList.prototype.get     = function(pos) {};
+SortedList.prototype.max     = function() {};
+SortedList.prototype.min     = function() {};
+SortedList.prototype.average = function() {};
+SortedList.prototype.sum     = function() {};
 
 module.exports = SortedList;
